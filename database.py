8558e7ec4e7cd3087b06687e0b81a90f491ee0db
Pierre VIVEN <pierreviven@gmail.com>
2026-04-20 02:35:07 +0200

    VF BDD en ligne

--- database.py
+++ database.py
@@ -15,7 +15,7 @@ DB_PASSWORD=os.getenv("DB_PASSWORD")
 def connexion_database():
     """ Connect to the PostgreSQL database server """
     try:
-        with psycopg2.connect(database = DB_NAME, user = DB_USER, host= DB_HOST,password = DB_PASSWORD,port = DB_PORT) as bdd:
+        with psycopg2.connect(database = DB_NAME, user = DB_USER, host= DB_HOST,password = DB_PASSWORD,port = DB_PORT, sslmode="require",) as bdd:
             return bdd
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
